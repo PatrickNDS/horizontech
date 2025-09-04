f87e6c23058ae2141739a565da2c0fff0d833627
Patrick Santos <patrick.santos@atlascopco.com>
2025-09-04 15:20:38 -0300

    Atualizações no header e remoção do invert

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Button } from '@/components/ui/button.jsx'
 import { motion } from 'framer-motion'
 import './App.css'
 
+// Import do logo
+import companyLogo from './assets/Logo-horizon-Preto-V2.png'
 
 function App() {
   // Variantes de animação para diferentes elementos
